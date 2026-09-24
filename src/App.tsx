4c7544d73d30288204c5357af97b9b1565f6e260
Ruan Victor <ruanvclima@gmail.com>
2026-09-24 16:29:21 -0300

    feat: created the conditions to winning or losing the game

--- src/App.tsx
+++ src/App.tsx
@@ -36,6 +36,7 @@ export default function App() {
       return null;
     }
     if (!letter.trim()) {
+      setLetter('');
       return alert('Digite uma letra');
     }
 
@@ -43,6 +44,7 @@ export default function App() {
     const exists = lettersUsed.find((used) => used.value.toUpperCase() === value);
 
     if (exists) {
+      setLetter('');
       return alert(`Você já utilizou a letra ${value}`);
     }
 
@@ -59,9 +61,32 @@ export default function App() {
     setLetter('');
   }
 
+  function endGame(message: string) {
+    alert(message);
+    startGame();
+  }
+
   useEffect(() => {
     startGame();
   }, []);
+
+  useEffect(() => {
+    if (!challenge) {
+      return;
+    }
+
+    setTimeout(() => {
+      if (score === challenge.word.length) {
+        return endGame('Parabéns, você descobriu a palavra');
+      }
+
+      const attemptLimit = challenge.word.length + ATTEMPT_MARGIN;
+
+      if (lettersUsed.length === attemptLimit) {
+        return endGame('Que pena, você usou todas as tentativas');
+      }
+    }, 200);
+  }, [score, lettersUsed.length]);
 
   if (!challenge) {
     return null;
