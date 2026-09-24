90b104c6d5551e801176df939815eed6c5c7f6ae
Ruan Victor <ruanvclima@gmail.com>
2026-09-24 05:41:21 -0300

    feat: created advinhe project and interface

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { Header } from './components/Header';
+import { Tip } from './components/Tips';
+import { Letter } from './components/Letter';
+import { Input } from './components/Input';
+import { Button } from './components/Button';
+import { LettersUsed } from './components/LettersUsed';
+import styles from './app.module.css';
+export default function App() {
+  function handleOnRestartGame() {
+    alert('Reiniciar o jogo');
+  }
+  return (
+    <div className={styles.container}>
+      <main>
+        <Header current={5} max={10} onRestart={handleOnRestartGame} />
+        <Tip tip="Uma das inguagem de programação mais utilizadas" />
+
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
+
+        <h4>Palpite</h4>
+
+        <div className={styles.guess}>
+          <Input autoFocus maxLength={1} placeholder="?" />
+          <Button title="Confirmar" />
+        </div>
+
+        <LettersUsed />
+      </main>
+    </div>
+  );
+}
